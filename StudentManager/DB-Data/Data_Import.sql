b33807fc49c724ff651f56d7626a590ea5f93525
ralph <syllable24@protonmail.com>
2019-01-19 09:30:20 +0100

    Testdaten für DB hinzugefügt

---
+++ StudentManager/DB-Data/Data_Import.sql
@@ -0,0 +1,27 @@
+LOAD DATA LOCAL INFILE '/home/ralph/Documents/FH/SWE02/StudentManager/DB-Data/Gruppen.csv'
+INTO TABLE TestSchema.Gruppen FIELDS TERMINATED BY ','
+ENCLOSED BY '"' LINES TERMINATED BY '\n';
+
+LOAD DATA LOCAL INFILE '/home/ralph/Documents/FH/SWE02/StudentManager/DB-Data/Kurse.csv'
+INTO TABLE TestSchema.Kurse FIELDS TERMINATED BY ','
+ENCLOSED BY '"' LINES TERMINATED BY '\n';
+
+LOAD DATA LOCAL INFILE '/home/ralph/Documents/FH/SWE02/StudentManager/DB-Data/Lehrer.csv'
+INTO TABLE TestSchema.Lehrer FIELDS TERMINATED BY ','
+ENCLOSED BY '"' LINES TERMINATED BY '\n';
+
+LOAD DATA LOCAL INFILE '/home/ralph/Documents/FH/SWE02/StudentManager/DB-Data/LogIns.csv'
+INTO TABLE TestSchema.LogIns FIELDS TERMINATED BY ','
+ENCLOSED BY '"' LINES TERMINATED BY '\n';
+
+LOAD DATA LOCAL INFILE '/home/ralph/Documents/FH/SWE02/StudentManager/DB-Data/Nutzerrechte.csv'
+INTO TABLE TestSchema.Nutzerrechte FIELDS TERMINATED BY ','
+ENCLOSED BY '"' LINES TERMINATED BY '\n';
+
+LOAD DATA LOCAL INFILE '/home/ralph/Documents/FH/SWE02/StudentManager/DB-Data/Studenten.csv'
+INTO TABLE TestSchema.Studenten FIELDS TERMINATED BY ','
+ENCLOSED BY '"' LINES TERMINATED BY '\n';
+
+LOAD DATA LOCAL INFILE '/home/ralph/Documents/FH/SWE02/StudentManager/DB-Data/Studiengaenge.csv'
+INTO TABLE TestSchema.Studiengaenge FIELDS TERMINATED BY ','
+ENCLOSED BY '"' LINES TERMINATED BY '\n';
